69829451b461360b5ea54a3b525d8f3776d58def
MrLongbottom <perich16@student.aau.dk>
2017-11-23 15:14:58 +0100

    Basic Menu Seup and started working on using panels as pages.

---
+++ WindowsFormsSolution/WindowsFormsSolution/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsSolution
+{
+    partial class FjordblinkMeetingProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FjordblinkMeetingProgram));
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.ProfileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MeetingsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ProjectsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UsersMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CustomersMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.ProfilePage = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MainMenu.SuspendLayout();
+            this.ProfilePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(1164, 0);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(100, 45);
+            this.SearchButton.TabIndex = 6;
+            this.SearchButton.TabStop = false;
+            this.SearchButton.Text = "Søg";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.MainMenu.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProfileMenuItem,
+            this.MeetingsMenuItem,
+            this.ProjectsMenuItem,
+            this.UsersMenuItem,
+            this.CustomersMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(1264, 45);
+            this.MainMenu.TabIndex = 4;
+            this.MainMenu.Text = "MainMenu";
+            // 
+            // ProfileMenuItem
+            // 
+            this.ProfileMenuItem.AutoSize = false;
+            this.ProfileMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ProfileMenuItem.Image")));
+            this.ProfileMenuItem.Name = "ProfileMenuItem";
+            this.ProfileMenuItem.Size = new System.Drawing.Size(120, 41);
+            this.ProfileMenuItem.Text = "Profil";
+            this.ProfileMenuItem.Click += new System.EventHandler(this.ProfileMenuItem_Click);
+            // 
+            // MeetingsMenuItem
+            // 
+            this.MeetingsMenuItem.Name = "MeetingsMenuItem";
+            this.MeetingsMenuItem.Size = new System.Drawing.Size(108, 41);
+            this.MeetingsMenuItem.Text = "Møder";
+            this.MeetingsMenuItem.Click += new System.EventHandler(this.MeetingsMenuItem_Click);
+            // 
+            // ProjectsMenuItem
+            // 
+            this.ProjectsMenuItem.Name = "ProjectsMenuItem";
+            this.ProjectsMenuItem.Size = new System.Drawing.Size(135, 41);
+            this.ProjectsMenuItem.Text = "Projekter";
+            // 
+            // UsersMenuItem
+            // 
+            this.UsersMenuItem.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.UsersMenuItem.Name = "UsersMenuItem";
+            this.UsersMenuItem.Size = new System.Drawing.Size(121, 41);
+            this.UsersMenuItem.Text = "Brugere";
+            // 
+            // CustomersMenuItem
+            // 
+            this.CustomersMenuItem.Name = "CustomersMenuItem";
+            this.CustomersMenuItem.Size = new System.Drawing.Size(114, 41);
+            this.CustomersMenuItem.Text = "Kunder";
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchBar.Location = new System.Drawing.Point(908, 7);
+            this.SearchBar.MaxLength = 300;
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(250, 30);
+            this.SearchBar.TabIndex = 5;
+            // 
+            // ProfilePage
+            // 
+            this.ProfilePage.Controls.Add(this.listBox1);
+            this.ProfilePage.Controls.Add(this.textBox1);
+            this.ProfilePage.Controls.Add(this.button1);
+            this.ProfilePage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProfilePage.Location = new System.Drawing.Point(0, 45);
+            this.ProfilePage.Name = "ProfilePage";
+            this.ProfilePage.Size = new System.Drawing.Size(1264, 717);
+            this.ProfilePage.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "as",
+            "asf",
+            "f",
+            "fa",
+            "fsa",
+            "sad",
+            "sad",
+            "sad",
+            "saf"});
+            this.listBox1.Location = new System.Drawing.Point(387, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 147);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // FjordblinkMeetingProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 762);
+            this.Controls.Add(this.ProfilePage);
+            this.Controls.Add(this.SearchBar);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.MainMenu);
+            this.Name = "FjordblinkMeetingProgram";
+            this.Text = "Fjorblink Meeting Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.ProfilePage.ResumeLayout(false);
+            this.ProfilePage.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem MeetingsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ProjectsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UsersMenuItem;
+        private System.Windows.Forms.TextBox SearchBar;
+        private System.Windows.Forms.ToolStripMenuItem CustomersMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ProfileMenuItem;
+        private System.Windows.Forms.Panel ProfilePage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsSolution/WindowsFormsSolution/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSolution
+{
+    public partial class FjordblinkMeetingProgram : Form
+    {
+        public FjordblinkMeetingProgram()
+        {
+            InitializeComponent();
+            currPanel = ProfilePage;
+        }
+
+        private Panel currPanel;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProfileMenuItem_Click(object sender, EventArgs e)
+        {
+            currPanel.Visible = false;
+            currPanel = ProfilePage;
+            currPanel.Visible = true;
+        }
+
+        private void MeetingsMenuItem_Click(object sender, EventArgs e)
+        {
+            currPanel.Visible = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
